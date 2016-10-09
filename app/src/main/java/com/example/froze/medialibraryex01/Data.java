12a671a02e7e6128d0153538efd774cd9ae7e63a
Hyojung Kim <Hyojung Kim>
2016-10-09 13:25:14 +0900

    android Media library ex01

---
+++ app/src/main/java/com/example/froze/medialibraryex01/Data.java
@@ -0,0 +1,13 @@
+package com.example.froze.medialibraryex01;
+
+import java.util.ArrayList;
+
+/**
+ * Created by froze on 2016-10-09.
+ */
+
+public class Data {
+
+    ArrayList<MusicData> observers = new ArrayList<>();
+
+}
---
+++ app/src/main/java/com/example/froze/medialibraryex01/MusicData.java
@@ -0,0 +1,15 @@
+package com.example.froze.medialibraryex01;
+
+/**
+ * Created by froze on 2016-10-09.
+ */
+
+public class MusicData {
+
+    String musicId;
+    String albumId;
+    String title;
+    String artist;
+    int image;
+
+}
